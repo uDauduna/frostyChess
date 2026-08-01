2ce9a8671e92dd3732a9ee3ffe85b11293da75c5
uDauduna <116510163+uDauduna@users.noreply.github.com>
2026-07-31 22:23:50 -0500

    board implementation

--- game/board.py
+++ game/board.py
@@ -10,6 +10,51 @@ class Board:
                       ["R", "N", "B", "Q", "K", "B", "N", "R"],
                       ]
 
+        self.ranks = {0:8,
+                      1:7,
+                      2:6,
+                      3:5,
+                      4:4,
+                      5:3,
+                      6:2,
+                      7:1,
+                      }
+        
+        self.files = {0:"a",
+                      1:"b",
+                      2:"c",
+                      3:"d",
+                      4:"e",
+                      5:"f",
+                      6:"g",
+                      7:"h",
+                      }
+        
+
+    def get_board(self):
+        """
+        Return either the FEN or PGN 
+        """
+        return
+
+    def change_state(self, piece, old_pos, new_pos):
+
+        """
+        Change the state base on the move
+        """
+        if legal(new_pos):
+            self.board[old_pos[0]][old_pos[1]] = "."
+            self.board[new_pos[0]][new_pos[1]] = piece
+        else:
+            """
+            We can't raise an error
+            """
+            print("Illegal Move")
+
+        return
+
+    
+
 board = Board()
 
 print(board.board)
