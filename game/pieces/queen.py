90a1a7934b341128c0f29d5e94a9af1062a409b5
uDauduna <116510163+uDauduna@users.noreply.github.com>
2026-08-02 23:53:17 -0400

    queen moves

--- game/pieces/queen.py
+++ game/pieces/queen.py
@@ -6,6 +6,92 @@ class Queen:
     def move(self):
         return
 
+    def eligible_moves(self, board):
+        self.eligible = []
+        row, col = self.row, self.col
+        while row < 8 :
+            row += 1
+            if board.square_is_empty((row, col)):
+                self.eligible.append((row, col))
+            else:
+                if board.pieces[row][col].color != self.color:
+                    self.eligible.append((row, col))
+                break
+        row, col = self.row, self.col
+        while row >= 0:
+            row -= 1
+            if board.square_is_empty((row, col)):
+                self.eligible.append((row, col))
+            else:
+                if board.pieces[row][col].color != self.color:
+                    self.eligible.append((row, col))
+                break
+        row, col = self.row, self.col
+        while col < 8:
+            col += 1 
+            if board.square_is_empty((row, col)):
+                self.eligible.append((row, col))
+            else:
+                if board.pieces[row][col].color != self.color:
+                    self.eligible.append((row, col))
+                break
+        row, col = self.row, self.col
+        while col >= 0:
+            col -= 1 
+            if board.square_is_empty((row, col)):
+                self.eligible.append((row, col))
+            else:
+                if board.pieces[row][col].color != self.color:
+                    self.eligible.append((row, col))
+                break
+        row, col = self.row, self.col
+        while (row < 8 and col < 8):
+            row += 1
+            col += 1 
+            if board.square_is_empty((row, col)):
+                self.eligible.append((row, col))
+            else:
+                if board.pieces[row][col].color != self.color:
+                    self.eligible.append((row, col))
+                break
+        row, col = self.row, self.col
+        while (row >= 0 and col >= 0):
+            row -= 1
+            col -= 1 
+            if board.square_is_empty((row, col)):
+                self.eligible.append((row, col))
+            else:
+                if board.pieces[row][col].color != self.color:
+                    self.eligible.append((row, col))
+                break
+        row, col = self.row, self.col
+        while (row >= 0 and col < 8):
+            row -= 1
+            col += 1 
+            if board.square_is_empty((row, col)):
+                self.eligible.append((row, col))
+            else:
+                if board.pieces[row][col].color != self.color:
+                    self.eligible.append((row, col))
+                break
+        row, col = self.row, self.col
+        while (row < 8 and col >= 0):
+            row += 1
+            col -= 1 
+            if board.square_is_empty((row, col)):
+                self.eligible.append((row, col))
+            else:
+                if board.pieces[row][col].color != self.color:
+                    self.eligible.append((row, col))
+                break
+        return self.eligible
+
+    def is_move_legal(self, pos, board):
+        self.eligible = self.eligible_moves(board)
+        if pos in self.eligible:
+            return True
+        return False
+
     def update_position(self, new_pos):
         self.row, self.col = new_pos
         return
