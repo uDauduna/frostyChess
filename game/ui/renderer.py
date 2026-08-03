8142312b14049e5b4370ff71e7162801c9f9fd76
uDauduna <116510163+uDauduna@users.noreply.github.com>
2026-08-03 18:21:28 -0400

    draw the board

--- game/ui/renderer.py
+++ game/ui/renderer.py
@@ -1,14 +1,35 @@
 # Example file showing a circle moving on screen
 import pygame
 
+#constants
+SCREEN_WIDTH = 1280
+SCREEN_HEIGHT = 720
+SQUARE_SIZE = 80
+BOARD_SIZE = 640
+# DARK_SQUARE = (92, 64, 51)
+DARK_SQUARE = "black"
+LIGHT_SQUARE = "grey"
+BACKGROUND_COLOR = "navy"
+
 # pygame setup
+
+
 pygame.init()
 screen = pygame.display.set_mode((1280, 720))
+board = pygame.Surface((BOARD_SIZE + 4, BOARD_SIZE + 4))
 clock = pygame.time.Clock()
 running = True
 dt = 0
 
-player_pos = pygame.Vector2(screen.get_width() / 2, screen.get_height() / 2)
+def draw_board():
+    for row in range(8):
+        for col in range(8):
+            if row%2 == 0:
+                color = LIGHT_SQUARE if (row * 8 + col)%2 == 0 else DARK_SQUARE
+            else:
+                color = DARK_SQUARE if (row * 8 + col)%2 == 0 else LIGHT_SQUARE
+            square = pygame.Rect(2+col* SQUARE_SIZE,2 + row* SQUARE_SIZE, SQUARE_SIZE, SQUARE_SIZE)
+            pygame.draw.rect(board, color, square)
 
 while running:
     # poll for events
@@ -18,19 +39,11 @@ while running:
             running = False
 
     # fill the screen with a color to wipe away anything from last frame
-    screen.fill("purple")
-
-    pygame.draw.circle(screen, "red", player_pos, 40)
-
-    keys = pygame.key.get_pressed()
-    if keys[pygame.K_w]:
-        player_pos.y -= 300 * dt
-    if keys[pygame.K_s]:
-        player_pos.y += 300 * dt
-    if keys[pygame.K_a]:
-        player_pos.x -= 300 * dt
-    if keys[pygame.K_d]:
-        player_pos.x += 300 * dt
+    screen.fill(BACKGROUND_COLOR)
+    board.fill("white")
+    draw_board()
+    screen.blit(board, (318, 38))
+    
 
     # flip() the display to put your work on screen
     pygame.display.flip()
