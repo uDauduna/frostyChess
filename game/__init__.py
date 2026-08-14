30b9feaf160c6a1eea1328b178ebb44224aae7f3
uDauduna <116510163+uDauduna@users.noreply.github.com>
2026-08-14 13:35:34 -0400

    module manager file restructure

---
+++ game/__init__.py
@@ -0,0 +1,9 @@
+from .board import Board
+from .chess_game import ChessGame
+from .move import Move
+
+__all__ = [
+    "Board",
+    "ChessGame",
+    "Move",
+]
---
+++ ui/__init__.py
@@ -0,0 +1,9 @@
+from .renderer import Renderer
+from .sprite import PieceSprite
+from .promotion_ui import PromotionUI
+
+__all__ = [
+    "Renderer",
+    "PieceSprite",
+    "PromotionUI",
+]
