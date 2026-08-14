48998e10153da9179ed6e687959a8a93a1351e1d
uDauduna <116510163+uDauduna@users.noreply.github.com>
2026-08-14 13:37:41 -0400

    delete player class

--- game/player.py
+++
@@ -1,7 +0,0 @@
-class Player:
-    def __init__(self, color):
-        self.color = color
-        self.special_moves = {"castling"}
-        self.check = False
-        self.castle_legal = True
-        self.timer = True
