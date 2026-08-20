499d934af7c970c0a01506aa913e8d4a1b05f594
uDauduna <116510163+uDauduna@users.noreply.github.com>
2026-08-20 10:44:32 -0400

    refactor: add en_passant and castling rights to move object

--- game/move.py
+++ game/move.py
@@ -9,6 +9,9 @@ class Move:
     promotion: str = None
     is_castling: bool = False
     is_en_passant: bool = False
+    promotion: str | None = None
+    previous_en_passant_target: tuple | None = None
+    previous_castling_rights: dict | None = None
 
     @property
     def resets_fifty_move_counter(self) ->bool:
