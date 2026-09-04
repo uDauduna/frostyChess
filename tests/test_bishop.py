dc5ac9da7438086df9a67cabc459d6135b669185
uDauduna <116510163+uDauduna@users.noreply.github.com>
2026-09-04 12:09:32 -0400

    refactor<test pieces>: improve readability

--- tests/test_bishop.py
+++ tests/test_bishop.py
@@ -3,9 +3,6 @@ from game.board import Board
 
 def test_bishop_blocked_at_start():
     board = Board()
-
     bishop = board.get_piece((7, 2))
-
     moves = bishop.pseudo_legal_moves(board)
-
     assert moves == []
