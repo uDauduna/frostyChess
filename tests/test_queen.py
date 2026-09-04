a998e27ab7c99a66a1713d0f1ccfb5237477b19c
uDauduna <116510163+uDauduna@users.noreply.github.com>
2026-09-04 12:09:59 -0400

    refactor<test pieces>: improve readability

--- tests/test_queen.py
+++ tests/test_queen.py
@@ -3,9 +3,6 @@ from game.board import Board
 
 def test_queen_blocked_at_start():
     board = Board()
-
     queen = board.get_piece((7, 3))
-
     moves = queen.pseudo_legal_moves(board)
-
     assert moves == []
