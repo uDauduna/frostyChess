98664a5a74aab50449875a7e6be77796536e99c1
uDauduna <116510163+uDauduna@users.noreply.github.com>
2026-09-08 12:30:41 -0400

    update game init file

--- game/__init__.py
+++ game/__init__.py
@@ -1,9 +1,6 @@
 from .board import Board
 from .chess_game import ChessGame
 from .move import Move
+from .pgn import export_pgn, import_pgn
 
-__all__ = [
-    "Board",
-    "ChessGame",
-    "Move",
-]
+__all__ = ["Board", "ChessGame", "Move", "export_pgn", "import_pgn"]
